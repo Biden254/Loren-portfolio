5da313e7e637f07a2abd281908ec62f0bd3a7678
Biden254 <159169843+Biden254@users.noreply.github.com>
2025-05-16 16:38:03 +0300

    My website portfolio

---
+++ script.js
@@ -0,0 +1,21 @@
+const words = [
+    "Cloud Practitioner",
+    "Cloud Developer",
+    "Cloud Architect",
+    "Cloud Engineer",
+    "Cloud Consultant"
+];
+
+let index = 0; // Start with the first word
+const typingText = document.querySelector(".typing-text span");
+
+function changeWord() {
+    typingText.textContent = words[index]; // Update the text content
+    index = (index + 1) % words.length; // Move to the next word, loop back to the start
+}
+
+// Change the word every 2 seconds
+setInterval(changeWord, 2000);
+
+// Initialize with the first word
+changeWord();
