c680bb3909255fe8dc5c06637386c604fa139ec1
Biden254 <159169843+Biden254@users.noreply.github.com>
2026-01-11 12:48:03 +0300

    Fixing render and gmail compatibility

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === 'production') {
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
-  secure: false, // true for 465, false for other ports
+  secure: process.env.SECURE === 'true', // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
